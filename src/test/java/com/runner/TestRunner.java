34fa1fccd6e8c17ebba4b9f94922f440406fbd6d
MOHANAPREETA <cmohanapreeta@gmail.com>
2025-09-13 16:15:46 +0530

    updated

--- src/test/java/com/runner/TestRunner.java
+++ src/test/java/com/runner/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-    features = "src/test/resources/Features",
+    features = "src/test/resources/features",
     glue = "com.step_definitions",
     plugin = {"pretty", "html:reports/cucumber-html-report.html"}
 )
